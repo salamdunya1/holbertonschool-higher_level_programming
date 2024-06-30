51cffc6602c1e8bf6a6ae82d17179e37e5faf29f
salamdunya1 <burmurdack@gmail.com>
2024-06-30 23:43:28 +0400

    mysql

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This script connects to a MySQL database
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from model_city import City
+
+Base = declarative_base()
+
+
+class State(Base):
+    """A class that represents a state"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True, unique=True)
+    name = Column(String(128), nullable=False)
