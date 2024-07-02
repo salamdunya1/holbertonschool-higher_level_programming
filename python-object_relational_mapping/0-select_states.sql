c858f8f2bb53ea04775933540073a84b0ce66574
salamdunya1 <158586631+salamdunya1@users.noreply.github.com>
2024-07-02 14:31:22 +0400

    Update 0-select_states.sql

--- python-object_relational_mapping/0-select_states.sql
+++ python-object_relational_mapping/0-select_states.sql
@@ -1,10 +1,17 @@
--- MySQL
-
+-- Create database if not exists
 CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
+
+
 USE hbtn_0e_0_usa;
-CREATE TABLE IF NOT EXISTS states ( 
-	    id INT NOT NULL AUTO_INCREMENT, 
-	    name VARCHAR(256) NOT NULL,
-	    PRIMARY KEY (id)
-	);
-	INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+
+
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+INSERT INTO states (name) VALUES
+("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+
