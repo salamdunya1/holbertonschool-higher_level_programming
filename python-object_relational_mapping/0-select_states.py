cb7e1a57c1624cd44925683d21252410c79618be
salamdunya1 <158586631+salamdunya1@users.noreply.github.com>
2024-07-02 14:31:50 +0400

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,13 @@
--- Create database if not exists
-CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa.
+"""
+import sys
+import MySQLdb
 
-
-USE hbtn_0e_0_usa;
-
-
-CREATE TABLE IF NOT EXISTS states (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
-);
-
-
-INSERT INTO states (name) VALUES
-("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states")
+    [print(state) for state in c.fetchall()]
 
